8a584ae2c15f0484bfbd045e0e59723d15b7e7bb
Elia-levi <95618753+Elia-levi@users.noreply.github.com>
2022-01-31 00:07:11 +0200

    ok

--- src/vodComps/vodItem.js
+++ src/vodComps/vodItem.js
@@ -6,7 +6,7 @@ function VodItem(props){
     return(
         <div key={item.imdbID} className='col-lg-3 col-md-4  p-2 text-light'>
         <div className='p-2 center '>
-            <img src={item.Poster} className='imgBord ' height={300} alt='{item.Title}'/>
+            <img src={item.Poster} className='imgBord ' height={300} alt={item.Title}/>
             <h5 className=' px-3  py-2'>{item.Title}</h5>
             <div className='d-flex align-items-center justify-content-around '>
             <button className='  btnHome'>More info</button>
