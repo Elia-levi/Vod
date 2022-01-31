502421628200771a6d293b730ead63c86ff6ca5e
Elia-levi <95618753+Elia-levi@users.noreply.github.com>
2022-01-31 19:14:51 +0200

    ok

--- src/vodComps/videoInfo.js
+++ src/vodComps/videoInfo.js
@@ -4,17 +4,8 @@ import axios from 'axios';
 
 
 function VideoInfo (props){
-    let param = useParams();
 
-    useEffect(() => {
-        doApi(param.YYYY);
-    }, [param])
 
-    const doApi = async (_param) => {
-        let url = `https://www.omdbapi.com/i=&apikey=8662ecc3`;
-        let resp = await axios.get(url);
-        setAr(resp.data.Search);
-    }
     return (
         <div className='container'>
 
