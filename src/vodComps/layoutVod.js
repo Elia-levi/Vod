c18ca52902987c6440c4fd955c19147f6cd5b8ae
Elia-levi <95618753+Elia-levi@users.noreply.github.com>
2022-01-31 12:52:15 +0200

    ok

--- src/vodComps/layoutVod.js
+++ src/vodComps/layoutVod.js
@@ -9,6 +9,7 @@ function LayoutVod(props) {
       <main  className='container-fluid  pb-4'>
         <Outlet />
       </main>
+      
       <footer className='bgFooter text-light container-fluid  text-center p-5 '>
         <div>the best movie in the world</div>
       </footer>
