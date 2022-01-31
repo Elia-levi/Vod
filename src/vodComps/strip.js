f104b50d880e220a29f9d369c156863caa24f1b1
Elia-levi <95618753+Elia-levi@users.noreply.github.com>
2022-01-31 19:01:42 +0200

    ok

--- src/vodComps/strip.js
+++ src/vodComps/strip.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Strip(props) {
     return (
         <div className='strip' style={{backgroundImage:"url(/images/movie.jpg)"}}>
-
+            <h1 className='center  shado'>movie</h1>
 
         </div>)
 }
