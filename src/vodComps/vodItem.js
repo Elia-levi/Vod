c9ec38dd49915a4e38c1bd3eed3310ddfc87b30c
Elia-levi <95618753+Elia-levi@users.noreply.github.com>
2022-02-01 17:45:15 +0200

    ok

--- src/vodComps/vodItem.js
+++ src/vodComps/vodItem.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function VodItem(props){
     let item = props.item;
@@ -6,11 +7,12 @@ function VodItem(props){
     return(
         <div key={item.imdbID} className='col-lg-3 col-md-4  p-2 text-light'>
         <div className='p-2 center '>
-        {(item.Poster!=="N/A")?<img  src={item.Poster} className='img-bord ' height={300} alt={item.Title}/>:
-        <img  src={"https://images.pexels.com/photos/356079/pexels-photo-356079.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"} className='img-bord ' height={300} alt={item.Title}/>}
+        {(item.Poster!=="N/A")? <Link to={"/video/"+item.imdbID}> <img  src={item.Poster} className='img-bord ' height={300} alt={item.Title}/></Link>
+        : <Link to={"/video/"+item.imdbID}> <img  src={"https://images.pexels.com/photos/356079/pexels-photo-356079.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"} className='img-bord ' height={300} alt={item.Title}/> </Link>}
+
             <h4 className='fs-6 px-3  py-2'>{item.Title}</h4>
             <div className='d-flex align-items-center justify-content-around '>
-            <button className='  btn-home'>More info</button>
+            <Link className=' btn-home' to={"/video/"+item.imdbID} >More info</Link>
             <small className='text-decoration-underline'>{item.Year}</small>
             </div>
         </div>
